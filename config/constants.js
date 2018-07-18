fbe836906fbeef6c399c03a620353c5ae4134128
Jake Cooley <jake@ideasbynature.com>
2018-07-18 21:17:34 +0300

    updated help section

--- config/constants.js
+++ config/constants.js
@@ -17,8 +17,20 @@ export const negativeLow = ':seriously:'
 export const negativeTiny = ':downtrend:'
 export const precision = 8
 export const help =
-    `   cc [symbol]
-    cc chart [symbol] [time]
+    `   *Coinbot's help section*
+    
+    cc [symbol] - [symbol]'s price in USD and amount relative to BTC
+    _e.g._  \`cc btc\`
+    
+    cc [symbol1] in [symbol2] - [symbol1]'s price in USD and amount relative to [symbol2]
+    _e.g._  \`cc btc in eth\`
+    
+    cc [symbol1], [symbol2], [symbol...] Send in an array to get multiple values at once
+    _e.g._  \`cc btc, eth, ada\`  \`cc btc, eth, ada in eth\`
+   
+   *BETA*
+       
+    cc chart [symbol] - Chart for [symbol] including price and market cap.
     `
 export const chartTimeFrame = {
     1: '1day/',
