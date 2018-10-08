1eb1e4c4eee3bce02a4c2016ee47e1f39ee55c7a
Jacob Cooley <j.cooley42@gmail.com>
2018-10-08 11:58:28 +0400

    Rebased with master

--- config/memes.js
+++ config/memes.js
@@ -1,3 +1,18 @@
 export const memes = [
-    ''
+    'sean',
+    'dylan'
+]
+export const people = [
+    'chris',
+    'cj',
+    'phil',
+    'jeff',
+    'justin',
+    'sean',
+    'jake',
+    'patrick',
+    'david',
+    'dylan',
+    'stu',
+    'steven'
 ]
